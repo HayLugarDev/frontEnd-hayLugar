c88e1800ff53d2149ce2451d136561234852a4f3
FACUNDO DIAZ <facundodg1000@gmail.com>
2025-07-09 13:46:28 +0000

    apiService.js edited online with Bitbucket

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// ConfiguraciÃ³n base de Axios
+// Configuración base de Axios
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://haylugar.com.ar/api",
   headers: {
     'Content-Type': 'application/json',
   }
