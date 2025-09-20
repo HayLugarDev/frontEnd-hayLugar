7fa7ac256779cedb0cd425cb9597961379b808c3
ExeVizuara <vizuara01@gmail.com>
2025-09-20 00:14:33 -0300

    Mejoras UI SpaceCard, detailSpace

--- src/components/pages/dashboardPage/SpaceCard.vue
+++ src/components/pages/dashboardPage/SpaceCard.vue
@@ -1,20 +1,20 @@
 <template>
     <router-link :to="`/espacio/${espacio.id}`">
         <div class="bg-secondary rounded-xl transition-all h-full lg:h-80 min-h-[22rem] lg:p-2 md:p-0">
-            <div class="relative aspect-square overflow-hidden rounded-lg p-4">
+            <div class="relative aspect-square overflow-hidden rounded-lg p-2 lg:p-1">
                 <Carousel :images="espacio.images" class="w-full h-full object-cover" :controls="false" />
             </div>
             <div class="p-1">
                 <div class="flex flex-col items-start px-2 md:px-0">
-                    <div class="text-lg font-bold text-primary line-clamp-1">
+                    <div class="text-xl md:text-xs  font-bold text-primary line-clamp-1">
                         {{ capitalizeFirst(espacio.name) }}
                     </div>
 
-                    <p class="text-md text-gray-800 line-clamp-1">
+                    <p class="text-md md:text-xs text-gray-800 line-clamp-1">
                         {{ espacio.location.split(',')[1] }}
                     </p>
 
-                    <p class="text-xs text-gray-500 line-clamp-1">
+                    <p class="text-sm md:text-[10px] text-gray-500 line-clamp-1">
                         {{ espacio.location.split(',')[0] }}
                     </p>
 
@@ -23,11 +23,11 @@
                             <div v-for="v in espacio.vehicle_capacities" :key="v.type" class="p-1 border rounded-xl">
                                 <font-awesome-icon :icon="['fas', `${vehicleTypeTranslations[v.type]}`]"
                                     class="text-gray-700 text-sm md:text-sm" />
-                                <p v-if="v.price_per_hour" class="text-[.6rem] font-normal md:hidden">Hora: ${{
-                                    v.price_per_hour.toLocaleString() }}</p>
+                                <p v-if="v.price_per_hour" class="text-[.6rem] font-normal md:hidden"> ${{
+                                    v.price_per_hour.toLocaleString() }}/h</p>
                             </div>
                         </div>
-                        <div class="text-gray-800 font-semibold text-md md:text-sm">
+                        <div class="text-gray-800 font-semibold text-xl md:text-sm">
                             <span class="text-yellow-600">★</span> {{ Number(espacio.average_rating).toFixed(1) }}
                         </div>
                     </div>
@@ -35,13 +35,13 @@
 
                 <div class="col-span-2 mb-2">
                     <div
-                        class="bg-blue-100 text-primary rounded-lg px-4 py-2 text-start text-[10px] font-semibold">
+                        class="bg-blue-100 text-primary rounded-lg p-2 text-start text-md md:text-[10px] font-semibold">
                         <div v-if="disponibilidad.dateRange && disponibilidad.dateRange.length === 2">
                             <span>
                                 {{ formatDate(disponibilidad.dateRange[0]) }} al {{ disponibilidad.dateRange[1] }}
                             </span>
                         </div>
-                        <div class="text-[10px]">
+                        <div>
                             <span>Horario: </span>
                             <span v-if="disponibilidad.start && disponibilidad.end">
                                 {{ disponibilidad.start }} a {{ disponibilidad.end }} hs
